1c37148f65ddeaf1f0921b723a375f1ae3546a43
Mobin-Dan <68151748+Mobin-Dan@users.noreply.github.com>
2020-10-18 22:33:27 +0330

    Create install.py

---
+++ install.py
@@ -0,0 +1,9 @@
+import os
+print("Download.....")
+os.system("apt update")
+os.system("pip install colorama")
+os.system("pkg install python2")
+os.system("pkg install python3")
+os.system("pip install requests")
+os.system("pip install socket")
+print(";)")
