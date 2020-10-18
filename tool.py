5d900ebe199031108376b5f74f3b807047116cbd
Mobin-Dan <68151748+Mobin-Dan@users.noreply.github.com>
2020-10-18 22:30:37 +0330

    Create tool.py

---
+++ tool.py
@@ -0,0 +1,32 @@
+import os
+from colorama import init, AnsiToWin32, Fore, Back
+print(Fore.CYAN ,"1:[*webscaning*]")
+print(Fore.CYAN ,"2:[*port scaning*]")
+print(Fore.RED ,"3:[*DOS*]")
+print(Fore.GREEN ,"4:[*console_hack_insta*]")
+print(Fore.RED ,"5:[*pass_combo_proxy*]")
+print("6:[mobindan]")
+mobindan =input('$')
+if mobindan== 1:
+        os.system("git clone https://github.com/Mobin-Dan/web_informations")
+        os.system("cd web_informations")
+        os.system("python host.py")
+if mobindan==2 :
+        os.system("git clone https://github.com/Mobin-Dan/Port_scaner")
+        os.system("cd Port_scaner")
+        os.system("python2 ViV.py")
+if mobindan== 3:
+        os.system("git clone https://github.com/Mobin-Dan/Dos")
+        os.system("cd Dos")
+        os.system("python2 dos.py")
+if mobindan== 4:
+        os.system("git clone https://github.com/Mobin-Dan/console_crack_insta")
+        os.system("cd console_crack_insta")
+        os.system("python2 mobin_dan_console.py")
+if mobindan== 5:
+        os.system("https://github.com/Mobin-Dan/pass_combo_proxy")
+        os.system("cd pass_combo_proxy")
+        os.system("ls")
+if mobindan== 6:
+        print(Fore.CYAN ,"FΘᄂᄂΘw ME::")
+        print(Fore.GREEN , "mr_mobin_dan")
